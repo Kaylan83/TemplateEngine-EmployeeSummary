23ed19a50be83d918bd89ab1ad59879e79fdf33a
Murad Atta <murad.atta@gmail.com>
2020-07-19 20:40:31 -0400

    initial working code

---
+++ Develop/app.js
@@ -0,0 +1,75 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const Question = require("./lib/Question");
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+// new created employees array
+const employees = [];
+
+// creating a manager function
+ manager = ()=> {
+
+   inquirer.prompt(Question.managerQuestions) 
+   .then((response) => {  
+    employees.push(new Manager(response.name, response.id, response.email, response.officeNumber))
+    newEmployee();
+    });
+}
+
+// creating engineer function
+newEngr = ()=> {
+
+        inquirer.prompt(Question.EngrQuestions)
+        .then((response) => { employees.push(new Engineer(response.name, response.id, response.email, response.github))
+        newEmployee();
+        });
+}
+
+//creating intern function
+newIntern = () => {
+    inquirer.prompt(Questions.InternQuestion)
+    .then((response) => {employees.push(new Intern(response.name, response.id, response.email, response.school))
+    newEmployee();
+    });
+}
+
+// creating the employee function and push the employee to the employees array 
+// then append it to the html file 
+ newEmployee = () => {
+     inquirer.prompt({
+         type: "list",
+         name: "role",
+         choices: [
+             "Engineer",
+             "Intern",
+             "Don't add more employees"
+         ]
+     }).then((response) => {
+         if (response.role ==="Engineer"){
+             newEngr();
+         } else if (response.role ==="Intern"){
+             newIntern();
+         } else {
+             checkIfDirectoryExist(OUTPUT_DIR);
+             fs.writeFileSync(outputPath,render(employees), err => {
+                 if (err) throw err;
+             });
+         }
+     });
+ };
+
+manager();
+
+// check if the outboot directory is available, if not created it
+checkIfDirectoryExist = (folder) => {
+    try { fs.statSync(folder)} catch (error) { fs.mkdirSync(folder)}
+}
---
+++ Develop/lib/Question.js
@@ -0,0 +1,84 @@
+const Questions = { 
+    
+    managerQuestions: [ 
+    {
+    type: "input",
+    name: "name",
+    message: "Please enter manager's name: "
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "Pleas enter manager's id: "
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "PLease enter manager's Email: "
+    },
+
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter manager's office number: "
+    }
+
+],
+
+ EngrQuestions: [ 
+    {
+    type: "input",
+    name: "name",
+    message: "Please enter engineer's name: "
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "Pleas enter engineer's id: "
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "PLease enter engineer's Email: "
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter engineer's github username: "
+    }
+
+],
+
+ InternQuestion: [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter Intern's name: "
+        },
+    
+        {
+            type: "input",
+            name: "id",
+            message: "Pleas enter Intern's id: "
+        },
+    
+        {
+            type: "input",
+            name: "email",
+            message: "PLease enter Intern's Email: "
+        },
+    
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter Intern's school name: "
+        }
+]
+};
+
+module.exports = Questions;
